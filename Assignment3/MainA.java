b42bc64a6a49d15d15fc8b108bc868795dbb1715
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-08-08 22:24:54 +0530

    Create MainA.java

---
+++ Assignment3/MainA.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MainA
+{
+    public static void main (String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter three nos:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int x;
+        if(a>b && a>c)
+            x=a;
+        else if(b>a && b>c)
+            x=b;
+        else 
+            x=c;
+        
+        System.out.println("Greatest number:"+x);
+        
+    }
+}
