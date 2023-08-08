9ccb26a08309339f47b86550f386c675ba0eda80
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-08-08 20:50:16 +0530

    Create Main3.java

---
+++ Assignment1/Main3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main3
+{
+    public static void main (String[] args) 
+    {
+        if(args.length>0)
+            for(int i=0;i<args.length;i++)
+                System.out.print(args[i]+" ");
+        else
+            System.out.println("No input in command line");
+    }
+}
