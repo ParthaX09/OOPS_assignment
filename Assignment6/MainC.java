755c47d49e20f151141d81053778a2b1d8c39443
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-01 22:10:28 +0530

    Update MainC.java

--- Assignment6/MainC.java
+++ Assignment6/MainC.java
@@ -21,18 +21,15 @@ class MainC
 
             switch(ch)
             {
-                case 1:boolean swapped;
-                       for (int i = 0; i < n - 1; i++) 
+                case 1:for (int i = 0; i < n - 1; i++) 
                        {
-                            swapped = false;
                             for (int j = 0; j < n - i - 1; j++) 
                             {
                                 if (arr[j] > arr[j + 1]) 
                                 {
                                     int temp = arr[j];
                                     arr[j] = arr[j + 1];
-                                    arr[j + 1] = temp;
-                                    swapped = true; 
+                                    arr[j + 1] = temp; 
                                 }
                             }
                         }
