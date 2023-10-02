9bcbe120cd8d543e97b798fa24c67e5c965fb024
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-02 10:23:32 +0530

    Update Main3.java

--- Assignment1/Main3.java
+++ Assignment1/Main3.java
@@ -1,11 +1,11 @@
-import java.util.Scanner;
+
 public class Main3
 {
     public static void main (String[] args) 
     {
         if(args.length>0)
-            for(int i=0;i<args.length;i++)
-                System.out.print(args[i]+" ");
+            for(String s: args)
+                System.out.print(s+" ");
         else
             System.out.println("No input in command line");
     }
