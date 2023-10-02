46be51653570c3fce32f87c55cbf03abf77873f7
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-02 20:19:54 +0530

    Update MainC.java

--- Assignment4/MainC.java
+++ Assignment4/MainC.java
@@ -4,17 +4,19 @@ public class MainC
     public static void main (String[] args) 
     {
         Scanner sc=new Scanner(System.in);
-        System.out.print("Enter a no.:");
+        System.out.print("Enter the range:");
+        int m=sc.nextInt();
         int n=sc.nextInt();
-        int c=0;
-        for(int i=2;i<Math.sqrt(n);i++)
-            if(n%i==0)
-                c++;
-        
-        if(c==0)
-            System.out.println(n+" is prime number");
-        else 
-            System.out.println(n+" is not prime number");
+        for(int k=m;k<=n;k++)
+        {
+            int c=0;
+            for(int i=2;i<k;i++)
+                if(k%i==0)
+                    c++;
+            if(c==0 && k!=1)
+                System.out.println(k);       
+        }
+        sc.close();
         
     }
 }
