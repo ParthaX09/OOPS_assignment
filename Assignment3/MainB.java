d66baec67bba2283400f443ba4aa5cf9d9893aa2
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-02 17:19:44 +0530

    Update MainB.java

--- Assignment3/MainB.java
+++ Assignment3/MainB.java
@@ -9,19 +9,25 @@ public class MainB
         int b=sc.nextInt();
         int c=sc.nextInt();
         double d=Math.pow(b,2.0)-4.0*a*c;
+        double x1,x2;
         if(d>0)
         {
-            double x1=(-b+Math.sqrt(d))/(2.0*a);
-            double x2=(-b-Math.sqrt(d))/(2.0*a);
+            x1=(-b+Math.sqrt(d))/(2.0*a);
+            x2=(-b-Math.sqrt(d))/(2.0*a);
             System.out.println("Two distinct real roots exist:"+x1+" "+x2);
         }
         else if(d==0)
         {
-            double x=-b/(2.0*a);
-            System.out.println("There is one real root:"+x);
+            x1=-b/(2.0*a);
+            System.out.println("There is one real root:"+x1);
         }
         else    
-            System.out.println("roots are imaginary");
-        
+        {
+            x1=-b/(2.0*a);
+            x2=Math.sqrt(d*-1)/(2.0*a);
+            System.out.println("The imaginary roots are: ("+x1+"+"+x2+"i)\tand\t"+"("+x1+"-"+x2+"i)");
+        }
+
+        sc.close();
     }
 }
