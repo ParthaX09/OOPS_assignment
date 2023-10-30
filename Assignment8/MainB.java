a95987498f2eccce44018ab68f4f4b7668223a11
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-30 22:46:11 +0530

    Create MainB.java

---
+++ Assignment8/MainB.java
@@ -0,0 +1,52 @@
+abstract class Number 
+{
+    protected int value;
+
+    public Number(int value) 
+    {
+        this.value = value;
+    }
+
+    public abstract void displayNum();
+}
+
+class HexNum extends Number 
+{
+    public HexNum(int value) 
+    {
+        super(value);
+    }
+
+    @Override
+    public void displayNum() 
+    {
+        System.out.println("Hexadecimal: " + Integer.toHexString(value));
+    }
+}
+
+class OctalNum extends Number 
+{
+    public OctalNum(int value) 
+    {
+        super(value);
+    }
+
+    @Override
+    public void displayNum() 
+    {
+        System.out.println("Octal: " + Integer.toOctalString(value));
+    }
+}
+
+public class MainB 
+{
+    public static void main(String[] args) 
+    {
+        Number num1 = new HexNum(255);  
+        Number num2 = new OctalNum(63);  
+        
+
+        num1.displayNum();
+        num2.displayNum();
+    }
+}
