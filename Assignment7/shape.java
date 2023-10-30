3973be9098b88c252397ddc6ecd235cc0607ddfa
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-30 10:17:42 +0530

    Update shape.java

--- Assignment7/shape.java
+++ Assignment7/shape.java
@@ -1,8 +1,11 @@
 public class shape
 {
-    public static void rect(int n, char ch) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+    public static void rect(int n, char ch) 
+    {
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
                 System.out.print(ch + " ");
             }
             System.out.println();
@@ -10,9 +13,12 @@ public class shape
     }
 
     
-    public static void rect(int l, int b, char ch) {
-        for (int i = 0; i < l; i++) {
-            for (int j = 0; j < b; j++) {
+    public static void rect(int l, int b, char ch) 
+    {
+        for (int i = 0; i < l; i++) 
+        {
+            for (int j = 0; j < b; j++) 
+            {
                 System.out.print(ch + " ");
             }
             System.out.println();
@@ -21,9 +27,9 @@ public class shape
     
     public static void main(String[] args) 
     {
-        
+        System.out.println("Filled Square");
         rect(5, '*');          
-        System.out.println();
+        System.out.println("\nFilled Rectangle");
         rect(4, 7, '#');       
     }
 }
