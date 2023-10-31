9cecfe60849060106226c7c499abe98e235b4c27
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-10-31 23:50:02 +0530

    Create MainF.java

---
+++ Assignment8/MainF.java
@@ -0,0 +1,35 @@
+interface Animal 
+{
+    void makeSound();
+}
+
+
+interface Vehicle 
+{
+    void start();
+}
+
+class AnimalVehicle implements Animal, Vehicle 
+{
+    @Override
+    public void makeSound() 
+    {
+        System.out.println("The animal makes a sound.");
+    }
+
+    @Override
+    public void start() 
+    {
+        System.out.println("The vehicle starts.");
+    }
+}
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        AnimalVehicle creature = new AnimalVehicle();
+        creature.makeSound();
+        creature.start();
+    }
+}
