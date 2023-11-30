5f451e82040158819705a462f93f752a278819f4
Partha Sarathi Hazra <94391232+ParthaX09@users.noreply.github.com>
2023-11-30 17:40:47 +0530

    Update MainA.java

--- Assignment10/MainA.java
+++ Assignment10/MainA.java
@@ -1,48 +1,39 @@
-class A extends Thread
-{
-    public void run()
-    {
-        try
-        {
-            for(int i=1;i<=10;i++)
-                System.out.println(i);
-        }
-        catch(Exception e)
-        {
-            System.out.println("Exception caught");
-        
-        } 
-    } 
-}
+public class MainA {
+    public static void main(String[] args) {
+        // Thread to print numbers from 1 to 10
+        Thread thread1 = new Thread(() -> {
+            
+                try 
+                {
+                    for (int i = 1; i <= 10; i++) 
+                    {
+                        System.out.println("Thread 1: " + i);
+                    }
+                } 
+                catch (Exception e) 
+                {
+                    e.printStackTrace();
+                }
+            });
 
-class B extends Thread
-{
-    public void run()
-    {
-        for(int i=1;i<=10;i++)
-        {
-            System.out.println(i);
-            try
-            {
-                if(i==6)
-                    Thread.sleep(10000);
-            } 
-            catch(Exception e) 
-            {
-                System.out.println("Exception caught");
+        // Thread to print numbers from 10 to 1
+        Thread thread2 = new Thread(() -> {
+            for (int i = 10; i >= 1; i--) {
+                System.out.println("Thread 2: " + i);
+
+                // If the value is 6, sleep for 10000 milliseconds (10 seconds)
+                if (i == 6) {
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        }
-    }
-}
+        });
 
-
-class MainA
-{
-    public static void main(String args[])
-    {
-        A x=new  A();
-        B y=new B();
-        x.start();
-        y.start(); 
+        // Start both threads
+        thread1.start();
+        thread2.start();
     }
 }
